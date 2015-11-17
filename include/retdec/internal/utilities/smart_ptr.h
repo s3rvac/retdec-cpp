1171f1cdd4d6dfdf00e5a8be16c0298aee7d9f81
Petr Zemek <s3rvac@gmail.com>
2015-11-17 19:43:10 +0100

    Fix indentation in smart_ptr.h.
    We should consistently used tabs for indentation.

--- include/retdec/internal/utilities/smart_ptr.h
+++ include/retdec/internal/utilities/smart_ptr.h
@@ -21,7 +21,7 @@ namespace internal {
 ///
 template <typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args &&... args) {
-    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
 ///
